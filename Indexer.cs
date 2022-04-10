973a63a56975c41bfec73ab6181af81d82d4bc7d
alexzbykovskiy <alexzbykovskiy@KINDERAB10>
2022-04-10 02:43:05 +0200

    Add project files.

---
+++ Indexer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PocketGoogle
+{
+    public class Indexer : IIndexer
+    {
+        public Dictionary<int, List<string>> IdsDoc = new Dictionary<int, List<string>>();
+        Dictionary<int, List<int>> pos = new Dictionary<int,List<int>>();
+        public void Add(int id, string documentText)
+        {
+            List<string> words = new List<string>();
+            string word = "";
+           List<int> posit = new List<int>();
+            for (int a = 0; a < documentText.Length; a++)
+            {
+                if (char.IsLetter(documentText[a])) word += documentText[a];
+                else if (word.Length > 0)
+                {
+                    words.Add(word);
+                    posit.Add(a-word.Length);
+                    word = "";
+                }
+                if (a == documentText.Length - 1)
+                {
+                    words.Add(word);
+                    posit.Add(documentText.Length - word.Length);
+                }
+            }
+            IdsDoc.Add(id, words);
+            pos.Add(id, posit);
+
+        }
+
+        public List<int> GetIds(string word)
+        {
+            List<int> ids = new List<int>();
+            foreach (var a in IdsDoc.Keys)
+            {
+                if (IdsDoc[a].Contains(word))
+                    ids.Add(a);
+            }
+            return ids;
+        }
+
+        public List<int> GetPositions(int id, string word)
+        {
+            List<int> posit = new List<int>();
+            int i = 0;
+            foreach (var a in IdsDoc[id]) {
+                if (a == word)
+                    posit.Add(pos[id][i]);
+                i++;
+            }
+            return posit;
+
+        }
+
+        public void Remove(int id)
+        {
+            IdsDoc.Remove(id);
+            pos.Remove(id);
+        }
+    }
+}
---
+++ Test/Indexer_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PocketGoogle;
+
+namespace PocketGoogleTests
+{
+    [TestFixture]
+    public class Indexer_Tests
+    {
+        private Dictionary<int, string> dictionary = new Dictionary<int, string>()
+            {
+                { 0, "A B C" },
+                { 1, "B C" },
+                { 2, "A C A C" },
+                { 3, "F, f ff" }
+            };
+        private readonly Indexer i = new Indexer();
+
+        [Test]
+        [Order(00)]
+        public void Add()
+        {
+            var actual = true;
+            foreach (var d in dictionary)
+                i.Add(d.Key, d.Value);
+            Assert.AreEqual(true, actual);
+        }
+       
+        [TestCase("C", new int[3] { 0, 1, 2 })]
+        [TestCase("X", new int[0])]
+        [Order(01)]
+        public void GetIds(string word, int[] expected)
+        {
+            var actual = i.GetIds(word);
+            Assert.AreEqual(expected.ToList(), actual);
+        }
+
+        [TestCase("A", 0, new int[1] { 0 })]
+        [TestCase("A", 2, new int[2] { 0, 4})]
+        [Order(02)]
+        public void GetPositions(string word, int id, int[] expected)
+        {
+            var actual = i.GetPositions(id, word);
+            Assert.AreEqual(expected.ToList(), actual);
+        }
+
+        [TestCase("A", 1, new int[2] { 0, 2 })]
+        [TestCase("A", 0, new int[1] { 2 })]
+        [TestCase("A", 2, new int[0])]
+        [Order(03)]
+        public void Remove(string word, int id, int[] expected)
+        {
+            i.Remove(id);
+            var actual = i.GetIds(word);
+            Assert.AreEqual(expected.ToList(), actual);
+        }
+
+        [TestCase("B", 1, new int[1] { 0 })]
+        public void Add_Remove_Add_GetPosition(string word, int id, int[] expected)
+        {
+            i.Add(id, word);
+            i.Remove(id);
+            i.Add(id, word);
+            var actual = i.GetPositions(id, word);
+            Assert.AreEqual(expected.ToList(), actual);
+        }
+    }
+}
